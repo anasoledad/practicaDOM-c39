f18dcf0288bb6b432489467f10f1bb5e4f1d96e1
Ana Rocha <anasoledad.rocha@gmail.com>
2023-03-28 02:20:15 -0300

    Cambiamos el color del título y el texto

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function cambiarTitulo(){
+    console.log('aqui quiero cambiar el titulo');
+    //traer un elemento del html
+    let tituloPrincipal = document.querySelector('h1');
+    //let tituloPrincipal = document.getElementsById('nombre');
+    //let tituloPrincipal = document.getElementsByClassName('container');
+    //let tituloPrincipal = document.getElementsByTagName('h1');
+    console.log(tituloPrincipal);
+    tituloPrincipal.className = 'display-4 text-primary';
+    tituloPrincipal.innerHTML = '<b>Título</b> modificado';
+
+}   
